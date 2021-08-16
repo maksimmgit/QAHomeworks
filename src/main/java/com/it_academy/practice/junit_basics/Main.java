f29564d12b7b0ef3374dbc426811cd645dedd0c1
maksim <maximum38@ya.ru>
2021-08-16 21:48:14 +0300

    homework3-fixed

--- src/main/java/com/it_academy/practice/junit_basics/Main.java
+++ src/main/java/com/it_academy/practice/junit_basics/Main.java
@@ -5,16 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int [] paramArray = Calculator.inputParameters();
 
-        int n1 = Integer.parseInt(sc.next());
-        int n2 = Integer.parseInt(sc.next());
-
-        Calculator calculator = new Calculator(n1, n2);
-
-        System.out.println("Plus result: " + calculator.calculate('+'));
-        System.out.println("Minus result: " + calculator.calculate('-'));
-        System.out.println("Division result: " + calculator.calculate('/'));
-        System.out.println("Multiply result: " + calculator.calculate('*'));
+        Calculator calculatorNew = new Calculator(paramArray);
+        System.out.println("Сложение: " + calculatorNew.calculateParams('+'));
+        System.out.println("Вычитание: " + calculatorNew.calculateParams('-'));
     }
 }
